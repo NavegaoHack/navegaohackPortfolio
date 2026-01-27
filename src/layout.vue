240c9a912500f194ca5439b5ebff6a25fde7315b
Navegado <navegao@sample.xyz>
2026-01-27 16:12:01 -0400

    Set profile github image and svg images

--- src/layout.vue
+++ src/layout.vue
@@ -1,50 +1,47 @@
 <script setup>
 
+const address = [
+    {
+        id: 1,
+        type: "Gmail",
+        val: "alejandro.cana.v",
+        img: "https://www.svgrepo.com/show/535366/envelope.svg"
+    },
+    {
+        id:2,
+        type: "Location",
+        val: "Nva. Esparta, Venezuela",
+        img: "https://www.svgrepo.com/show/522167/location.svg"
+    }
+]
+
 </script>
 
 <template>
     <div class="flex gap-4 p-4 items-start justify-center max-md:flex-col max-md:items-center">
         <div class="shrink-0 flex flex-col gap-4 items-center border p-4 w-80 max-md:w-full">
-            <div class="size-48 bg-gray-200"></div>
+            <img src="https://github.com/navegaohack.png" class="size-48"/>
             <div>
                 <h1 class="text-2xl font-bold text-center">NavegaoHack</h1>
                 <p class="text-lg text-center text-gray-600">Not so professional software developer</p>
             </div>
             <div class="h-0.5 w-full bg-gray-300"></div>
 
-            <div class="w-full p-4 border rounded-xl flex gap-2">
-                <img class="size-14 border-2" src="" alt="img">
+
+            <div v-for="data in address" :key="data.id" class="w-full p-4 border rounded-xl flex gap-2">
+                <img class="size-12 p-2" :src="data.img" alt="img">
                 <div class="w-0.5 bg-gray-300"></div>
                 <div>
-                <h2 class="text-xl">type</h2>
-                <p class="text-gray-600">12345678</p>
+                <h2 class="text-xl">{{data.type}}</h2>
+                <p class="text-gray-600">{{data.val}}</p>
                 </div>
             </div>
 
-
-            <div class="w-full p-4 border rounded-xl flex gap-2">
-                <img class="size-14 border-2" src="" alt="img">
-                <div class="w-0.5 bg-gray-300"></div>
-                <div>
-                <h2 class="text-xl">type</h2>
-                <p class="text-gray-600">12345678</p>
-                </div>
-            </div>
-
-
-            <div class="w-full p-4 border rounded-xl flex gap-2">
-                <img class="size-14 border-2" src="" alt="img">
-                <div class="w-0.5 bg-gray-300"></div>
-                <div>
-                <h2 class="text-xl">type</h2>
-                <p class="text-gray-600">12345678</p>
-                </div>
-            </div>
 
             <div class="flex gap-4">
-                <div class="size-4 bg-gray-300"></div>
-                <div class="size-4 bg-gray-300"></div>
-                <div class="size-4 bg-gray-300"></div>
+                <img class="size-4" src="https://www.svgrepo.com/show/501245/linkedin.svg"/>
+                <img class="size-4" src="https://www.svgrepo.com/show/513089/youtube-168.svg"/>
+                <img class="size-4" src="https://www.svgrepo.com/show/501210/github.svg"/>
             </div>
 
 
