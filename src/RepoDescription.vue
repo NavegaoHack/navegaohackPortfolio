d85c674615063804c1fac1a3d00e800c5e6f41b1
Navegado <navegao@sample.xyz>
2026-02-22 23:28:36 -0400

    UPDATE: finish basic markdown description via api for every repo

--- src/RepoDescription.vue
+++ src/RepoDescription.vue
@@ -1,14 +1,23 @@
 <script setup>
 import { defineProps } from 'vue';
-const props = defineProps(['info'])
+const props = defineProps(['info', 'viewable'])
+const emit = defineEmits(['viewable'])
+
+const closeReadme = () => {
+    emit('viewable', { 'value': false })
+}
 </script>
 
 <template>
-    <div class="absolute translate-x-4 translate-y-4">
+    <div class="absolute top-0">
 
-        <div class="size-4 grid place-content-center p-4 rounded-full bg-gray-400 text-white">x</div>
-        <div class="w-2/3 bg-white shadow-2xl flex flex-col gap-4 p-4 repo-description" v-html="props.info">
+        <div :class="{'scale-100 opacity-100': props.viewable}" class="transition-all opacity-25 scale-75 duration-500 flex justify-center p-8 w-screen">
+            <div v-if="props.viewable" class="w-2/3 bg-white shadow-2xl relative p-4 rounded-2xl">
+                <div class="absolute select-none right-4 size-4 grid place-content-center p-4 rounded-full bg-gray-400 text-white hover:scale-110 active:bg-gray-300" @click="closeReadme">x</div>
+                <div class="flex flex-col gap-4 p-4 repo-description" v-html="props.info">
 
+                </div>
+            </div>
         </div>
 
     </div>
--- src/layout.vue
+++ src/layout.vue
@@ -14,7 +14,7 @@ const openRepoDescription = (id) => {
 }
 
 const repoDescriptionText = ref("")
-
+const isRepoDescriptionViewable = ref(false)
 
 
 const address = [
@@ -44,6 +44,7 @@ const getReadmeFile = (path) => {
         .then(text => {
             repoDescriptionText.value = marked.parse(text)
             console.log(text)
+            isRepoDescriptionViewable.value = true
         })
 }
 
@@ -78,56 +79,59 @@ onMounted(async() => {
 </script>
 
 <template>
-    <div class="relative flex gap-4 p-4 items-start justify-center max-md:flex-col max-md:items-center">
-        <div class="flex flex-col gap-4 items-center rounded-xl  p-4 w-80 max-md:w-full">
-            <img src="https://github.com/navegaohack.png" class="size-48 rounded-xl shadow-lg"/>
-            <div>
-                <h1 class="text-2xl font-bold text-center">NavegaoHack</h1>
-                <p class="text-lg text-center text-gray-600">Not so professional software developer</p>
-            </div>
-            <div class="h-0.5 w-full bg-gray-300"></div>
-
-
-            <div v-for="data in address" :key="data.id" class="w-full p-4 bg-gray-100 shadow-lg rounded-xl flex gap-2">
-
-                <img class="size-12 p-2 dark:fill-sky-300" :src="data.img" alt="img"/>
-                <div class="w-0.5 bg-gray-300"></div>
+    <div class="relative">
+        <div class="flex p-4 gap-4 items-start justify-center max-md:flex-col max-md:items-center transition-opacity duration-700" :class="{'opacity-50': isRepoDescriptionViewable}">
+            <div class="flex flex-col gap-4 items-center rounded-xl  p-4 w-80 max-md:w-full">
+                <img src="https://github.com/navegaohack.png" class="size-48 rounded-xl shadow-lg"/>
                 <div>
-                <h2 class="text-xl">{{data.type}}</h2>
-                <p class="text-gray-600">{{data.val}}</p>
+                    <h1 class="text-2xl font-bold text-center">NavegaoHack</h1>
+                    <p class="text-lg text-center text-gray-600">Not so professional software developer</p>
                 </div>
-            </div>
+                <div class="h-0.5 w-full bg-gray-300"></div>
 
-            <div class="flex gap-4">
-                <img class="size-4" src="https://www.svgrepo.com/show/501245/linkedin.svg"/>
-                <img class="size-4" src="https://www.svgrepo.com/show/513089/youtube-168.svg"/>
-                <img class="size-4" src="https://www.svgrepo.com/show/501210/github.svg"/>
+
+                <div v-for="data in address" :key="data.id" class="w-full p-4 bg-gray-100 shadow-lg rounded-xl flex gap-2">
+
+                    <img class="size-12 p-2 dark:fill-sky-300" :src="data.img" alt="img"/>
+                    <div class="w-0.5 bg-gray-300"></div>
+                    <div>
+                    <h2 class="text-xl">{{data.type}}</h2>
+                    <p class="text-gray-600">{{data.val}}</p>
+                    </div>
+                </div>
+
+                <div class="flex gap-4">
+                    <img class="size-4" src="https://www.svgrepo.com/show/501245/linkedin.svg"/>
+                    <img class="size-4" src="https://www.svgrepo.com/show/513089/youtube-168.svg"/>
+                    <img class="size-4" src="https://www.svgrepo.com/show/501210/github.svg"/>
+                </div>
+
+
             </div>
+            <main class="p-4  bg-gray-100 w-2/3 max-md:w-full rounded-xl">
+                <h3 class="text-3xl text-center font-bold mb-4">Projects</h3>
+                <div class="flex flex-wrap justify-center gap-4">
+                    <div v-for="Project in Projects"
+                        :key=Project.id
+                        class="w-80 h-72 p-4 bg-gray-200 shadow-lg rounded-xl flex flex-col gap-2"
+                        @click="() => {
+        getReadmeFile(Project.full_name)                
+                        }">
+                        <div class="h-2/3 overflow-hidden rounded-xl">
+                            <img v-if="Project.hasThumbnail" :src="getRawUrlContent(Project.full_name)" alt="" class="bg-cover bg-top">
+                            <div v-else class="h-full  text-4xl grid text-gray-500 place-content-center"> <p>{{ Project.name }}</p> </div>
+                        </div>
+                        <h2 class="text-2xl font-bold">{{ Project.name }}</h2>
+                        <p class="text-gray-700">{{ Project.description }}</p>
+                    </div>
+                </div>
+                
+            </main>
 
 
         </div>
-        <main class="p-4  bg-gray-100 w-2/3 max-md:w-full rounded-xl">
-            <h3 class="text-3xl text-center font-bold mb-4">Projects</h3>
-            <div class="flex flex-wrap justify-center gap-4">
-                <div v-for="Project in Projects"
-                    :key=Project.id
-                    class="w-80 h-72 p-4 bg-gray-200 shadow-lg rounded-xl flex flex-col gap-2"
-                    @click="() => {
-    openRepoDescription(Project.id)
-    getReadmeFile(Project.full_name)                
-                    }">
-                    <div class="h-2/3 overflow-hidden rounded-xl">
-                        <img v-if="Project.hasThumbnail" :src="getRawUrlContent(Project.full_name)" alt="" class="bg-cover bg-top">
-                        <div v-else class="h-full  text-4xl grid text-gray-500 place-content-center"> <p>{{ Project.name }}</p> </div>
-                    </div>
-                    <h2 class="text-2xl font-bold">{{ Project.name }}</h2>
-                    <p class="text-gray-700">{{ Project.description }}</p>
-                </div>
-            </div>
-            
-        </main>
-
-        <RepoDescription :info="repoDescriptionText"/>
+        <RepoDescription @viewable="(r) => {isRepoDescriptionViewable = r.value}" :viewable="isRepoDescriptionViewable" :info="repoDescriptionText"/>
     </div>
+
 
 </template>
